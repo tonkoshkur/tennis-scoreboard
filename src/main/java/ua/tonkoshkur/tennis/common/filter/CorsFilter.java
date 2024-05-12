e8b86445765b4aa622222d856428709476ce9f29
Ilya Tonkoshkur <54292571+tonkoshkur@users.noreply.github.com>
2024-05-12 17:22:58 +0200

    feat: add CORS filter for allowing cross-origin requests

---
+++ src/main/java/ua/tonkoshkur/tennis/common/filter/CorsFilter.java
@@ -0,0 +1,21 @@
+package ua.tonkoshkur.tennis.common.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST");
+
+        chain.doFilter(request, response);
+    }
+}
